f769d16abae036e3ba1e99934b31abe76074076a
Bruno Ferreira <chalkos@chalkos.net>
2014-03-26 01:36:04 +0000

    adicionadas mais cenas, preciso de algoritmos de grafos

--- pontos.pl
+++ pontos.pl
@@ -1,26 +1,73 @@
 % -----------------------------------
 % flags
 
-%set_prolog_flag(single_var_warnings, off).
-
+% set_prolog_flag(single_var_warnings, off).
 
 % -----------------------------------
-% define as posições das várias lojas
+% define as posições dos vários pontos de interesse
 % posicao(nome, x, y)
 
-posicao(loja1, 0, 0).
-posicao(loja2, 2, 2).
-posicao(loja3, 4, 6).
-posicao(loja4, 5, 7).
-posicao(loja5, 6, 8).
-posicao(loja6, 7, 9).
-posicao(loja7, 8, 0).
+posicao(p1, 0, 0).
+posicao(p2, 2, 2).
+posicao(p3, 4, 6).
+posicao(p4, 5, 7).
+posicao(p5, 6, 8).
+posicao(p6, 7, 9).
+posicao(p7, 8, 0).
 
 % -----------------------------------
-% distancias entre as lojas
-% distancia(lojaA, lojaB, Resultado)
+% define serviços nos pontos de interesse
+% servico(nome_loja, nome_servico).
+
+servico(p1, McDonalds).
+servico(p2, Combustivel).
+servico(p3, Lavandaria).
+
+servico(p7, McDonalds).
+
+% -----------------------------------
+% define ligações entre pontos
+% arco( Ponto1, Ponto2 )
+
+arco(p1,p7).
+
+% -----------------------------------
+% predicado distancias entre as lojas
+% distancia(lojaA, lojaB, Resultado).
 
 distancia(A,B,R) :-
 	posicao(A,X1,Y1),
 	posicao(B,X2,Y2),
+	distancia(X1,Y1,X2,Y2,R).
+
+% -----------------------------------
+% predicado distancias entre pontos
+
+distancia(X1, Y1, X2, Y2, R) :-
 	R is sqrt( exp(X1 - X2,2) + exp(Y1 - Y2,2) ).
+
+% -----------------------------------
+% predicado ponto de interesse próximo
+% pontoProximo( X, Y, distanciaMax, Ponto ).
+
+pontoProximo( X, Y, 0, P ) :-
+	posicao(P, X, Y).
+pontoProximo( X, Y, M, P ) :-
+	posicao(P, A, B),
+	distancia( X, Y, A, B, N ),
+	N =< M.
+
+% -----------------------------------
+% predicado pontos numa regiao
+% orla( X, Y, distanciaMax, ListaPontos )
+
+% é preciso a cena de obter todas as soluções do pontoProximo
+
+% -----------------------------------
+% predicado ponto de interesse mais proximo
+% pontoMaisProximo( X, Y, P )
+
+% pontoMaisProximo( X, Y, P ) :-
+% mais próximo que quê?
+% usar uma lista de todas as distancias? é preciso ver o dijkstra que o 12 encontrou.
+
